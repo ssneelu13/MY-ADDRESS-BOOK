40b62b550cbe3732e1a05d4e8e007242038d818f
Neelufer <ssneelu13@gmail.com>
2023-11-22 13:35:12 +0530

    DEFINE THE PROPERTIES OF ADDRESS BOOK

---
+++ src/Address_Book_Package/Contact_Details.java
@@ -0,0 +1,65 @@
+package Address_Book_Package;
+
+public class Contact_Details {
+        //declare variables
+        private String First_name;
+        private String Last_name;
+        private String Address;
+        private String City;
+        private String State;
+        private String Zip_code;
+        private String Phone_number;
+        private String Email_id;
+
+
+        //use setters and getters to access the private variables
+        public void setFirst_name(String First_name) {
+            this.First_name = First_name;
+        }
+        public String getFirst_name() {
+            return First_name;
+        }
+        public void setLast_name(String Last_name) {
+            this.Last_name = Last_name;
+        }
+        public String getLast_name() {
+            return Last_name;
+        }
+        public void setAddress(String Address) {
+            this.Address = Address;
+        }
+        public String getAddress() {
+            return Address;
+        }
+        public void setCity(String City) {
+            this.City = City;
+        }
+        public String getCity() {
+            return City;
+        }
+        public void setState(String State) {
+            this.State = State;
+        }
+        public String getState() {
+            return State;
+        }
+        public void setZip_code(String Zip_code) {
+            this.Zip_code = Zip_code;
+        }
+        public String getZip_code() {
+            return Zip_code;
+        }
+        public void setPhone_number(String Phone_number) {
+            this.Phone_number = Phone_number;
+        }
+        public String getPhone_number() {
+            return Phone_number;
+        }
+        public void setEmail_id(String Email_id) {
+            this.Email_id = Email_id;
+        }
+        public String getEmail_id() {
+            return Email_id;
+        }
+    }
+
